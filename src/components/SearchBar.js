2e74c03cce3b1355f260720d4a9c749532630a56
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-06-24 11:45:20 -0600

    functionality works, responsive design left

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,12 +8,11 @@ const SearchBar = () => {
     const [mainCrypto, setMainCrypto] = useState('Bitcoin');
     const inputEl = useRef(null);
 
-    const handleClick = () => {
+    const handleSubmit = (e) => {
         if(inputEl.current.value){
             setMainCrypto(inputEl.current.value);
         }
         inputEl.current.value = '';
-        displaySearchBox();
     }
 
     function displaySearchBox() {
@@ -31,10 +30,12 @@ const SearchBar = () => {
             <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous"></link>
             <header className="search-bar">
                 <div className="search-box pheight">
-                    <div className="search-btn pheight" href="#" onClick={()=> {handleClick()}}>
-                        <i ref={inputEl} className="fa fa-search" ></i>
-                    </div>
-                    <input ref={inputEl} spellCheck="false" type="text" className="pheight" id="input-box" placeholder="Search for crypto" autoCapitalize="on" style={{width:'0px'}} ></input>
+                    <form action="" onSubmit={(e) => {handleSubmit()}}>
+                        <div className="search-btn pheight" href="#" onClick={()=> {displaySearchBox()}}>
+                            <i ref={inputEl} className="fa fa-search" ></i>
+                        </div>
+                        <input ref={inputEl} spellCheck="false" type="text" className="pheight" id="input-box" placeholder="Search for crypto" autoCapitalize="on" style={{width:'0px'}} ></input>
+                    </form>
                 </div>
             </header>
             <MainCrypto cryptoName={mainCrypto}/>
