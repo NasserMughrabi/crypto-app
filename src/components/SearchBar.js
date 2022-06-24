cf68998a42eca271169f7bc8155828ff823884e4
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-06-24 16:29:49 -0600

    fixed form bug e.preventDefault()

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,7 @@ const SearchBar = () => {
     const inputEl = useRef(null);
 
     const handleSubmit = (e) => {
+        e.preventDefault();
         if(inputEl.current.value){
             setMainCrypto(inputEl.current.value);
         }
