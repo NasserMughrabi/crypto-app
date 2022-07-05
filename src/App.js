3d352ba22ab25aaef7e4fbfda3221c797b09fb97
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-07-05 12:18:40 -0600

    Project Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
 import {BsArrowDownUp} from 'react-icons/bs'
+import icon from './favicon.png';
 
 function App() {
+
+  useEffect(() => {
+      const favicon = document.getElementById('favicon');
+      favicon.setAttribute('href', icon);
+  }, []);
+  
   return (
     <>
     <title><BsArrowDownUp/></title>
